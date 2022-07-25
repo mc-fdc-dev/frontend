c916db7a3dba03ac00109ec198f2b19f8d5887c5
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-26 08:06:39 +0900

    Create terms.tsx

---
+++ pages/terms.tsx
@@ -0,0 +1,11 @@
+import Content from "../components/content"
+import { ReactNode} from 'react'
+
+
+export default function Terms() {
+    return (
+        <Content>
+            <h2 className="text-3xl text-center">利用規約</h2>
+        </Content>
+    )
+}
