901fd64ac16ca62da1989f7df1b42d4e4bfc54e8
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-25 12:48:23 +0000

    push

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -16,22 +16,44 @@ function StaffMember({children}: Props) {
 }
 
 export default function Staff() {
+    const staffs = [
+        {
+            name: 'mc_fdc',
+            role: 'Owner',
+            intro: '式守さん大好きキッズです。',
+        },
+        {
+            name: 'tasuren',
+            role: 'Sub owner',
+            intro: '未記入',
+        },
+        {
+            name: 'devon',
+            role: 'Developer',
+            intro: '未記入',
+        }
+    ]
     return (
         <Content>
             <h2 className='text-3xl text-center'>スタッフ</h2>
             <p className='text-center text-xl'>スタッフは、サービスを改善するためにおります。</p>
             <div className='grid grid-cols-3'>
+                {staffs.map(staff => (
+                    <StaffMember key={staff.name}>
+                        <Image src={`/images/staff/${staff.name}.png`} alt={staff.name} width={300} height={300} />
+                        <h3 className='text-xl'>{staff.name}</h3>
+                        <p>{staff.intro}</p>
+                    </StaffMember>
+                ))}
                 <StaffMember>
                     <Image src="/images/staff/mc_fdc.png" alt="mc_fdc" width={300} height={300} />
-                    <p>mc_fdc(Owner)</p>
+                    <p className="text-xl">mc_fdc(Owner)</p>
+                    <p>式守さん大好きキッズです。</p>
                 </StaffMember>
                 <StaffMember>
                     <Image src="/images/staff/tasuren.png" alt="tasuren" width={300} height={300} />
-                    <p>tasuren(Sub owner)</p>
-                </StaffMember>
-                <StaffMember>
-                    <Image src="/images/staff/devon.png" alt="mc_fdc" width={300} height={300} />
-                    <p>devon(Member)</p>
+                    <p className="text-xl">tasuren(Sub owner)</p>
+                    <p>未記入</p>
                 </StaffMember>
             </div>
         </Content>
