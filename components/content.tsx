f4cfa87ccbab25459e430fccee95e5726bbda953
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-26 08:08:30 +0900

    Update content.tsx

--- components/content.tsx
+++ components/content.tsx
@@ -3,13 +3,15 @@ import Base from "./base"
 
 
 interface Props {
+    title: string;
     children: ReactNode;
 }
 
-export default function Content({children}: Props) {
+export default function Content({title, children}: Props) {
     return (
         <Base>
             <div className="mt-4 mx-[10%]">
+                <h2 className="text-3xl text-center">{title}</h2>
                 {children}
             </div>
         </Base>
