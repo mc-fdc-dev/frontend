53d65846744bedf3017ea2fb6739f65fd65075d1
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-27 08:11:56 +0900

    Update privacy-policy.tsx

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -10,7 +10,7 @@ interface Props {
 function Article({title, children}: Props) {
     return (
         <div className="py-2">
-            <h3 className="text-2xl">{title}</h3>
+            <h3 className="text-2xl font-bold">{title}</h3>
             {children}
         </div>
     )
