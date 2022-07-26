aae03e8c9a6b64834b716bbf79cb76bdc3d232f4
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-26 09:33:09 +0900

    Update terms.tsx

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -5,7 +5,7 @@ import { ReactNode} from 'react'
 export default function Terms() {
     return (
         <Content title="利用規約">
-            <div>
+            <div className="text-center">
                 <p>工事中</p>
             </div>
         </Content>
