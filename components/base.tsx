9f6d5e93102900a7a07909470585782693e02bf1
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-26 06:26:32 +0000

    ok

--- components/base.tsx
+++ components/base.tsx
@@ -3,7 +3,6 @@ import Nav from './nav'
 import { ReactNode } from "react"
 import Link from 'next/link'
 import Footer from './footer'
-import { motion } from 'framer-motion'
 
 
 interface Props {
@@ -19,19 +18,14 @@ export default function Base({children}: Props) {
                 <meta property='og:description' content='mc-fdc-devはapiやbotを提供します。' />
                 <meta property='og:type' content='website' />
             </Head>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}>
-                <div className="border-b-4 border-pink-300">
-                    <div className="flex mx-8 justify-between my-2 items-center">
-                        <Link href="/"><a><h1 className='text-4xl'>mc-fdc-dev</h1></a></Link>
-                        <Nav />
-                    </div>
+            <div className="border-b-4 border-pink-300">
+                <div className="flex mx-8 justify-between my-2 items-center">
+                    <Link href="/"><a><h1 className='text-4xl'>mc-fdc-dev</h1></a></Link>
+                    <Nav />
                 </div>
-                <main>{children}</main>
-                <Footer />
-            </motion.div>
+            </div>
+            <main>{children}</main>
+            <Footer />
         </>
     )
 }
