bc7bae61d0cdac1d2aaae6e5a31c2c27edc6a57a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-27 10:36:34 +0900

    Update content.tsx

--- components/content.tsx
+++ components/content.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Content({title, children}: Props) {
     return (
-        <div className="mt-4 mx-[10%]">
+        <div className="mt-4 mx-[10%] pb-8">
             <h2 className="text-3xl text-center">{title}</h2>
             {children}
         </div>
