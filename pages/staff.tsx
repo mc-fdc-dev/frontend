3cccfcbc5ca6ae8bd8809b04da22e156a8d50328
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-28 22:47:43 +0900

    Update staff.tsx

--- pages/staff.tsx
+++ pages/staff.tsx
@@ -54,7 +54,7 @@ export default function Staff() {
             <div className='grid grid-cols-1 md:grid-cols-3'>
                 {staffs.map(staff => (
                     <StaffMember key={staff.name}>
-                        <Image className="rounded-full" src={`/images/staff/${staff.name}.png`} alt={staff.name} width={300} height={300} />
+                        <Image className="rounded-full" src={`/images/staff/${staff.name}.webp`} alt={staff.name} width={300} height={300} />
                         <h3 className='text-xl'>{`${staff.name}(${staff.role})`}</h3>
                         <p>{staff.intro}</p>
                     </StaffMember>
