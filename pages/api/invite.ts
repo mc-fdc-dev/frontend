30a5c11ca4ba0ee334a22c44d7d037e3d2917470
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-04 21:01:04 +0900

    Create invite.ts

---
+++ pages/api/invite.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default handler(req: NextApiRequest, res: NextApiResponse) {
+    res.redirect("https://discord.com/api/oauth2/authorize?client_id=1002877676526239794&permissions=413058591808&scope=bot%20applications.commands")
+}
