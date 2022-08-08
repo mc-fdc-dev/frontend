36d87cc626741192b9bb2de5d293af531980e587
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-07 20:34:40 -1000

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -6,15 +6,15 @@ import Content from "../../components/content"
 const fetcher = (apiURL: string) => fetch(apiURL).then(res => res.json())
 
 function User() {
-    const { user } = useSWR("https://api.mc-fdc.com/dashboard/me", fetcher)
-    if (!user) return <p>Loading</p>
-    if (!user.success) {
+    const { data } = useSWR("https://api.mc-fdc.com/dashboard/me", fetcher)
+    if (!data) return <p>Loading</p>
+    if (!data.success) {
         return <a className="text-lg rounded bg-[#5865F2] p-2 text-white" href="https://api.mc-fdc.com/dashboard/login">ログイン</a>
     }
     return (
         <div>
-            <h3>{`${user.username}#${user.discriminator}`}</h3>
-            <Image className="rounded-full" alt={user.username} src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} width={300} height={300} />
+            <h3>{`${data.username}#${data.discriminator}`}</h3>
+            <Image className="rounded-full" alt={data.username} src={`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png`} width={300} height={300} />
         </div>
     )
 }
