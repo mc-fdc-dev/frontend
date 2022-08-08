9edb47e00364d118d5168a3c03029bb15c434774
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 05:05:49 -1000

    Update data.ts

--- interfaces/dashboard/data.ts
+++ interfaces/dashboard/data.ts
@@ -1,8 +1,10 @@
 import UserT from "./user.ts"
+import GuildT from "./guild.ts"
 
 
 export default interface DataT {
     status: boolean;
     message: string | null;
     user: UserT;
+    guilds: GuildT[];
 }
