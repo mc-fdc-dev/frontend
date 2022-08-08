0b8f2c9d00d5d3bced6e5075349a38ee0b68cd98
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 04:57:12 -1000

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -17,10 +17,11 @@ function User() {
     if (!data.status) {
         return <a className="text-lg rounded bg-[#5865F2] p-2 text-white" href="https://api.mc-fdc.com/dashboard/login">ログイン</a>
     }
+    const user = data.user
     return (
         <div className="flex flex-row">
-            <Image className="rounded-full" alt={data.username} src={`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png?size=128`} width={128} height={128} />
-            <h3>{`${data.username}#${data.discriminator}`}</h3>
+            <Image className="rounded-full" alt={user.username} src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=128`} width={128} height={128} />
+            <h3>{`${user.username}#${user.discriminator}`}</h3>
         </div>
     )
 }
