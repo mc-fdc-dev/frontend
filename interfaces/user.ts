cfd00aef54796673246053f750216011038ac0f6
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-07 21:54:11 -1000

    Update user.ts

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,17 +1,17 @@
 export default interface UserT {
     success: boolean;
     message: string | null;
-    ?id: string;
-    ?username: string;
-    ?avatar: string;
-    ?avatar_decoration: string | null;
-    ?discriminator: string;
-    ?public_flags: number;
-    ?flags: number;
-    ?banner: string | null;
-    ?banner_color: string | null;
-    ?accent_color: string | null;
-    ?locale: string;
-    ?mfa_enabled: string;
-    ?premium_type: boolean;
+    id?: string;
+    username?: string;
+    avatar?: string;
+    avatar_decoration?: string | null;
+    discriminator?: string;
+    public_flags?: number;
+    flags?: number;
+    banner?: string | null;
+    banner_color?: string | null;
+    accent_color?: string | null;
+    locale?: string;
+    mfa_enabled?: string;
+    premium_type?: boolean;
 }
