772a62ae51517fe674d95aacd06bcb6f34ab9dd2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 05:34:13 -1000

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -24,10 +24,10 @@ function UserAndGuild() {
                 <Image className="rounded-full" alt={user.username} src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=128`} width={128} height={128} />
                 <h3>{`${user.username}#${user.discriminator}`}</h3>
             </div>
-            <div>
+            <div className="grid grid-cols-3">
                 {data.guilds.map(guild => (
                     <div key={guild.id}>
-                        <Image className="rounded-full" alt={guild.name} src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} width={128} height={128} />
+                        <Image className="rounded-full" alt={guild.name} src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=128`} width={128} height={128} />
                         <p className="text-center">{guild.name}</p>
                     </div>
                 ))}
