211faf46c4c41420441c5dbca6580dd7dae0e092
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 06:33:23 -1000

    Update guild.ts

--- interfaces/dashboard/guild.ts
+++ interfaces/dashboard/guild.ts
@@ -1,7 +1,7 @@
 export default interface GuildT {
     id: string;
     name: string;
-    icon: string;
+    icon: string | null;
     owner: boolean;
     permissions: string;
     features: string[];
