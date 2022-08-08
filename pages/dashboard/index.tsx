dd40b695df4414486e08171ebd116537846db894
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 10:31:26 -1000

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -29,7 +29,8 @@ function UserAndGuild() {
             <h3 className="text-3xl text-center">サーバー</h3>
             <div className="grid grid-cols-3">
                 {data.guilds.map(guild => {
-                    return guild.owner ? (
+                    const bitPermissions = new PermissionsBitField(Number(guild.permissions));
+                    return bitPermissions.has(PermissionsBitField.Flags.Administrator) ? (
                         <div className="border rounded text-center m-4" key={guild.id}>
                             <Image className="rounded-full" alt={guild.name} src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=128`} width={128} height={128} />
                             <Link href={`https://mc-fdc.com/dashboard/${guild.id}`}><p className="text-center">{guild.name}</p></Link>
