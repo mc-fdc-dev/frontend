99a9a804543ab7f624981710f412efcb42804e7a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-08 04:09:04 -1000

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -13,15 +13,14 @@ async function fetcher(apiURL: string): Promise<UserT> {
 
 function User() {
     const { data } = useSWR("https://api.mc-fdc.com/dashboard/me", fetcher)
-    console.log(data);
     if (!data) return <p>Loading</p>
     if (!data.status) {
         return <a className="text-lg rounded bg-[#5865F2] p-2 text-white" href="https://api.mc-fdc.com/dashboard/login">ログイン</a>
     }
     return (
-        <div>
-            <h3>{`${data.username}#${data.discriminator}`}</h3>
+        <div className="flex">
             <Image className="rounded-full" alt={data.username} src={`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png`} width={100} height={100} />
+            <h3>{`${data.username}#${data.discriminator}`}</h3>
         </div>
     )
 }
