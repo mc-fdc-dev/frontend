c2bbd7fe6942f67cced7469eebf30c13dc1b7a0a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-18 17:19:09 +0900

    Update article.tsx

--- components/article.tsx
+++ components/article.tsx
@@ -17,7 +17,7 @@ interface Props {
 export default function Article({meta, children}: Props) {
     return (
         <Content title={meta.title}>
-            <div>
+            <div className="flex">
                 <Image className="rounded-full" src={`/images/staff/${meta.author}.webp`} alt={meta.author} width={30} height={30} />
                 <p>作成者: {meta.author}</p>
             </div>
