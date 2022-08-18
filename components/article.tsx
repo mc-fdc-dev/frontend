ae0c3e6eedb18fd7b222e6f6e93e7a475adaed6c
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-18 17:47:27 +0900

    Update article.tsx

--- components/article.tsx
+++ components/article.tsx
@@ -20,7 +20,7 @@ export default function Article({meta, children}: Props) {
             <h2 className="text-5xl text-left font-semibold py-2">{meta.title}</h2>
             <div className="flex py-2">
                 <Image className="rounded-full" src={`/images/staff/${meta.author}.webp`} alt={meta.author} width={40} height={40} />
-                <p className="py-[50%]">{meta.author}</p>
+                <p>{meta.author}</p>
             </div>
             <article className="prose pt-8">
                 {children}
