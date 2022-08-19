a78963dde3b092a621de9eaa7a841a07e8b9c7a1
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-19 19:51:15 +0900

    Update article.tsx

--- components/article.tsx
+++ components/article.tsx
@@ -18,7 +18,6 @@ interface Props {
 export default function Article({meta, children}: Props) {
     return (
         <div className="mt-4 mx-[10%] pb-8">
-            <h2 className="text-5xl text-left font-semibold py-2">{meta.title}</h2>
             <div className="flex justify-between">
                 <div className="flex py-2">
                     <Image className="rounded-full" src={`/images/staff/${meta.author}.webp`} alt={meta.author} width={45} height={45} />
@@ -26,6 +25,7 @@ export default function Article({meta, children}: Props) {
                 </div>
                 <small className="text-gray-500">{meta.time}</small>
             </div>
+            <h2 className="text-5xl text-left font-semibold py-2">{meta.title}</h2>
             <article className="prose pt-8">
                 {children}
             </article>
