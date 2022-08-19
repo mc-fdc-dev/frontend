d970c99cfd8ed4138dc5b6f44dc47fc358b68fec
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-19 20:02:15 +0900

    Update test.tsx

--- pages/articles/test.tsx
+++ pages/articles/test.tsx
@@ -1,10 +1,15 @@
 import { promises as fs } from 'fs'
 
 export async function getStaticProps() {
-    const files = await fs.readdir("./").filter(file => {
+    const articles = await fs.readdir("./").filter(file => {
         return file.isFile() && file.name.endsWith(".md")
     }).map(async file => {
         let article = require(`./${file.name}`)
         return article.meta
     })
+    return {
+        props: {
+            articles: articles
+        }
+    }
 }
